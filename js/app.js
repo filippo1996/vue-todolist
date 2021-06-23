518c647480060e83430fcfc6529b243c4d929cc9
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-06-23 16:13:05 +0200

    add logic todos + logic delete

--- js/app.js
+++ js/app.js
@@ -10,9 +10,16 @@ const app = new Vue(
         },
         methods:{
             saveTodo: function(){
+                if(!this.valueInput?.trim()){
+                    alert('Non puoi lasciare il campo vuoto');
+                    return false;
+                }
                 this.todosList.unshift(this.valueInput);
                 this.valueInput = '';
                 console.log(app.todosList);
+            },
+            deleteTodo: function(value){
+                this.todosList.splice(value, 1);
             }
         }
     }
